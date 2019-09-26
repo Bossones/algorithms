31416dbc3811ddab85c4db31e5e0e765b06725bb
Bossones <bogdan_boginskiy@mail.ru>
2019-09-26 23:37:26 +0300

    weighted quick union algorithm 1.33

---
+++ firstchapter/weightedquickunion/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//решение задачи свзяности двух вводимых пар
+//методом взвешенного быстрого объединения
+int main() {
+    const int N = 10;
+    int i, j, p, q, id[N], sz[N];
+        for (i = 0; i < N; ++i) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+        while (std::cin >> p >> q) {
+            for (i = p; i != id[i]; i = id[i]);
+            for (j = q; j != id[j]; j = id[j]);
+            if (i == j) {
+                std::cout << "Alredy exist" << std::endl;
+                continue;
+            }
+            if (sz[i] < sz[j]) {
+                id[i] = j;
+                sz[j] += sz[i];
+            } else {
+                id[j] = i;
+                sz[i] += sz[j];
+            }
+            std::cout << " " << p << " " << q << std::endl;
+        }
+    return 0;
+}
